538bc8b97f1e1a9df6e3ca86e2cbf27421070a6f
Jesszim <jess.zim04@gmail.com>
2024-08-14 09:48:03 -0700

    title-section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from './Components/Navbar';
+import Title from './Components/Title';
 import './App.css';
 
 function App() {
   return (
+    <div>
   <Navbar/>
+ <Title/>
+  </div>
   );
 }
 
---
+++ src/Components/Title.jsx
@@ -0,0 +1,26 @@
+import { BiLogoLinkedinSquare } from "react-icons/bi"
+import { VscGithubInverted } from "react-icons/vsc";
+import { TbBrandGmail } from "react-icons/tb";
+import computer from "../images/jessicacomp.png"
+import "./title.css"
+
+function Title() {
+  return (
+    <div id="title-container">
+      <div>
+        <img src={computer} alt="me" id="mypic" />
+      </div>
+      <div id="title-section">
+        <h1 id="myname">Jessica Zimmerman</h1>
+        <h2>Web Developer</h2>
+        <div id="contact-icons">
+          <a href="https://www.linkedin.com/in/jessica-zimmerman-7777b957/" id="linkedinicon" target="_blank"><BiLogoLinkedinSquare/></a>
+          <a href="https://github.com/Jesszim" target="_blank"><VscGithubInverted/></a>
+          <a href="mailto: jess.zim04@gmail.com" id="gmailicon" target="_blank"><TbBrandGmail/></a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Title
