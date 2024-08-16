7bff82a13765940554f8bab3cff5b62c5ac16b05
Jesszim <jess.zim04@gmail.com>
2024-08-16 09:17:19 -0700

    skills section

---
+++ src/images/skills/skills-logos.jsx
@@ -0,0 +1,16 @@
+import css from './css.svg'
+import git from './git.svg'
+import html from './html.svg'
+import js from './javascript.svg'
+import json from './json.svg'
+import mongo from './mongodb.svg'
+import node from './nodejs.svg'
+import npm from './npm.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import vs from './vscode.svg'
+import github from './github.svg'
+
+export{
+css, git, html, js, json, mongo, node, npm, react, redux, vs, github
+}
